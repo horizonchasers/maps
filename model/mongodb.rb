751426548f601eabc9e1d758b2f673a4c2101c43
horizonchasers <cwall@horizonchasers.com>
2014-03-07 23:05:15 -0500

    Tinga Mod

--- model/mongodb.rb
+++ model/mongodb.rb
@@ -5,7 +5,7 @@ require './model/venue'
 require './model/checkin'
 
 if ENV['RACK_ENV'] == 'production'
-  db = URI.parse(ENV['mongodb://webuser:tinga@172.31.44.116:27017/maps'])
+  db = URI.parse('mongodb://webuser:tinga@172.31.44.116:27017/maps')
   db_name = db.path.gsub(/^\//, '')
   DB = Mongo::Connection.new(db.host, db.port).db(db_name)
   DB.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
